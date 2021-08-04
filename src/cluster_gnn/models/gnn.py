f48af994f5e451bb28d3834f2f97e0d0ea13b9d1
Jacan Chaplais <jlc1n20@soton.ac.uk>
2021-08-04 18:16:58 +0100

    prototyping concat, and cleaning up scripts

--- src/cluster_gnn/models/gnn.py
+++ src/cluster_gnn/models/gnn.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 import torch
 import torchmetrics
 import torch_geometric as pyg
@@ -43,21 +45,57 @@ class Interaction(pyg.nn.MessagePassing):
 
 class Net(pl.LightningModule):
     def __init__(self, dim_node: int = 4, dim_edge: int = 0,
-                 dim_embed_edge: int = 64, dim_embed_node: int = 32,
-                 num_hidden: int = 3, final_bias: bool = False,
-                 pos_weight: float = 80.0,
-                 learn_rate: float = 1e-4, weight_decay: float = 5e-4,
+                 dim_embed_edge: int = 64, dim_embed_node: int = 64,
+                 dim1_node=None, dim1_edge=None,
+                 num_hidden: int = 7, final_bias: bool = True,
+                 pos_weight: float = 4.5,
+                 learn_rate: float = 1.0e-4,
+                 weight_decay: float = 3.0e-5,
                  infer_thresh: float = 0.5):
         super(Net, self).__init__()
         # define the architecture
-        self.encode = Interaction(dim_edge, dim_node,
-                                  dim_embed_edge, dim_embed_node)
-        self.message = pyg.nn.Sequential('x, edge_index, edge_attrs', [
-            (Interaction(dim_embed_edge, dim_embed_node,
-                         dim_embed_edge, dim_embed_node),
-             'x, edge_index, edge_attrs -> edge_attrs, x')
-             for i in range(num_hidden)
-             ])
+        self.dim_node = dim_node
+        self.dim_edge = dim_edge
+        self.dim_embed_node = dim_embed_node
+        self.dim_embed_edge = dim_embed_edge
+        self.__expand = lambda inp: list(chain.from_iterable(inp))
+
+        self.dims = { # True if in first layer
+            'edge': {
+                True: dim1_edge,
+                False: self.dim_embed_edge,
+                },
+            'node': {
+                True: dim1_node,
+                False: self.dim_embed_node,
+                }
+            }
+
+        self.__cat = lambda x1, x2: torch.cat([x1, x2], dim=1)
+
+        self.model = pyg.nn.Sequential('x_init, edge_index, edge_attrs',
+            [( # encoder
+                Interaction(
+                    self.dim_edge, self.dim_node,
+                    self.dims['edge'][dim1_edge != None],
+                    self.dims['node'][dim1_node != None]
+                    ),
+                'x_init, edge_index, edge_attrs -> edge_attrs, x'
+                ),
+            ]
+            + self.__expand([(
+                (self.__cat, 'x_init, x -> x_in'),
+                (Interaction(
+                    self.dims['edge'][i == 0 and dim1_edge != None],
+                    self.dims['node'][i == 0 and dim1_node != None],
+                    self.dim_embed_edge, self.dim_embed_node),
+                'x_in, edge_index, edge_attrs -> edge_attrs, x')
+                ) for i in range(num_hidden)])
+            )
+
+        print(self.dims)
+        print(self.model)
+
         self.classify = torch.nn.Linear(dim_embed_edge, 1, bias=final_bias)
         # optimiser args
         self.lr = learn_rate
@@ -66,22 +104,27 @@ class Net(pl.LightningModule):
         self.criterion = torch.nn.BCEWithLogitsLoss(
                 pos_weight=torch.tensor(pos_weight, device=self.device),
                 reduction='mean')
-        # add metrics
+        # METRICS:
+        # train
         self.train_ACC = torchmetrics.Accuracy(threshold=infer_thresh)
         self.train_F1 = torchmetrics.F1(
                 num_classes=1, threshold=infer_thresh)
+        # validate
         self.val_ACC = torchmetrics.Accuracy(threshold=infer_thresh)
         self.val_F1 = torchmetrics.F1(
                 num_classes=1, threshold=infer_thresh)
         self.val_PR = torchmetrics.BinnedPrecisionRecallCurve(
                 num_classes=1, num_thresholds=5)
+        # test
+        self.test_PR = torchmetrics.BinnedPrecisionRecallCurve(
+                num_classes=1)
+        self.test_ROC = torchmetrics.ROC(compute_on_step=False)
+        self.test_AUC = torchmetrics.AUROC()
 
     def forward(self, data, sigmoid=True):
         node_attrs, edge_attrs = data.x, data.edge_attr
-        edge_attrs, node_attrs = self.encode(node_attrs, data.edge_index,
-                                             edge_attrs)
-        edge_attrs, node_attrs = self.message(node_attrs, data.edge_index,
-                                              edge_attrs)
+        edge_attrs, node_attrs = self.model(node_attrs, data.edge_index,
+                                            edge_attrs)
         pred = self.classify(edge_attrs)
         if sigmoid:
             pred = torch.sigmoid(pred)
@@ -144,3 +187,28 @@ class Net(pl.LightningModule):
         for i, t in enumerate(thresh):
             self.log(f'ptl/val_prec_thresh_{t:.3f}', prec[i])
             self.log(f'ptl/val_recall_thresh_{t:.3f}', recall[i])
+
+    def test_step(self, batch, batch_idx):
+        edge_pred = self(batch, sigmoid=False)
+        loss = self.criterion(edge_pred, batch.y.view(-1, 1))
+        preds = torch.sigmoid(edge_pred)
+        target = batch.y.view(-1, 1).int()
+        self.test_ROC(preds, target)
+        self.test_AUC(preds, target)
+        # self.test_PR(preds, target)
+        return {'loss': loss,
+                'preds': preds,
+                'target': target}
+
+    def test_epoch_end(self, outputs):
+        # prec, rec, thresh = self.test_PR.compute()
+        roc = self.test_ROC.compute()
+        auc = self.test_AUC.compute()
+        out_dir = '/home/jlc1n20/projects/cluster_gnn/models/big/'
+        # torch.save(prec, out_dir + 'prec.pt')
+        # torch.save(rec, out_dir + 'rec.pt')
+        # torch.save(thresh, out_dir + 'thresh.pt')
+        torch.save(auc, out_dir + 'auc.pt')
+        torch.save(roc, out_dir + 'roc.pt')
+        # self.log('ptl/test_roc', self.test_ROC.compute())
+        # self.log('ptl/test_auc', self.test_AUC.compute())
