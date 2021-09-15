e744e85e30c71a2b7813165d7f58fd6d5a42a012
Jacan Chaplais <jlc1n20@soton.ac.uk>
2021-09-15 11:48:56 +0100

    adhoc changes for sub script flexibility before python interface

--- scripts/sub.sh
+++ scripts/sub.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
-#SBATCH --partition=ecsstaff
+#SBATCH --partition=ecsall
 #SBATCH --account=ecsstaff
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
 #SBATCH --gres=gpu:1
 #SBATCH --time=01:00:00
-#SBATCH --job-name=ui
-#SBATCH --output=/home/jlc1n20/projects/cluster_gnn/log/%x-%j.out
+#SBATCH --job-name=ignn
+#SBATCH --output=log/%x-%j.out
 #SBATCH --gres-flags=enforce-binding
 
+if [ -z $1 ] ; then
+    echo "you must supply a config file"
+    exit 2
+else
+    configfile=$1
+fi
+
+# filesystem
+projdir=$(git rev-parse --show-toplevel)
+progfile=$projdir/src/cluster_gnn/models/train_model.py
+
+if [ -z "${DEV+x}" ] ; then
+    conda_ptg=ptg
+else
+    if [[ $projdir == *"$(readlink -m $DEV)"* ]] ; then
+        conda_ptg=ptg
+    else
+        conda_ptg=ptg_prod
+    fi
+fi
+
 # setting up environment
-source activate ptg
+source activate $conda_ptg
 # export CUDA_VISIBLE_DEVICES='0,1,2,3'
 export NCCL_IB_DISABLE=1
 export NCCL_DEBUG=INFO
 export PYTHONFAULTHANDLER=1
 
-# filesystem
-projdir=$HOME/projects/cluster_gnn
-progfile=$projdir/src/cluster_gnn/models/train_model.py
-
 # execution
-python $progfile -c $projdir/configs/train/dev.yml
+python $progfile -c $configfile
